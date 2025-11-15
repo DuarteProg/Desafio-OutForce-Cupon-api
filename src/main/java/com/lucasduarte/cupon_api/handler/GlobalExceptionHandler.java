6f5876b0968fc4dd023e2c14174c521bb2b18902
Lucas <lucas.duartebarbosa@outlook.com>
2025-11-15 14:14:46 -0300

    Feat: Criação da classe GlobalExceptionHandler

---
+++ src/main/java/com/lucasduarte/cupon_api/handler/GlobalExceptionHandler.java
@@ -0,0 +1,40 @@
+package com.lucasduarte.cupon_api.handler;
+
+import com.lucasduarte.cupon_api.exception.CouponNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(CouponNotFoundException.class)
+    public ResponseEntity<Object> handleCouponNotFound(CouponNotFoundException ex) {
+
+        Map<String, Object> error = new HashMap<>();
+        error.put("timestamp", LocalDateTime.now());
+        error.put("status", HttpStatus.NOT_FOUND.value());
+        error.put("error", "Not Found");
+        error.put("message", ex.getMessage());
+
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
+    }
+
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<Object> handleGeneralException(Exception ex) {
+
+        Map<String, Object> error = new HashMap<>();
+        error.put("timestamp", LocalDateTime.now());
+        error.put("status", HttpStatus.INTERNAL_SERVER_ERROR.value());
+        error.put("error", "Internal Server Error");
+        error.put("message", "Erro inesperado no servidor.");
+
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(error);
+    }
+}
