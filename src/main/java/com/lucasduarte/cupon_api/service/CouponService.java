f98acb1d651425bc1da1f798cb108ec2ad234443
Lucas <lucas.duartebarbosa@outlook.com>
2025-11-15 15:50:06 -0300

    Feat: Criação do método create na classe CouponService

--- src/main/java/com/lucasduarte/cupon_api/service/CouponService.java
+++ src/main/java/com/lucasduarte/cupon_api/service/CouponService.java
@@ -1,11 +1,13 @@
 package com.lucasduarte.cupon_api.service;
 
+import com.lucasduarte.cupon_api.dto.CouponRequestDTO;
 import com.lucasduarte.cupon_api.dto.CouponResponseDTO;
 import com.lucasduarte.cupon_api.exception.CouponNotFoundException;
 import com.lucasduarte.cupon_api.model.Coupon;
 import com.lucasduarte.cupon_api.repository.CouponRepository;
 import com.lucasduarte.cupon_api.utils.CouponMapper;
 import org.springframework.stereotype.Service;
+
 
 @Service
 public class CouponService {
@@ -16,9 +18,18 @@ public class CouponService {
         this.repository = repository;
     }
 
+
     public CouponResponseDTO findById(String id) {
         Coupon coupon = repository.findById(id)
                 .orElseThrow(() -> new CouponNotFoundException("Cupom não encontrado."));
+
+        return CouponMapper.toResponse(coupon);
+    }
+
+    public CouponResponseDTO create(CouponRequestDTO dto) {
+        Coupon coupon = CouponMapper.toEntity(dto);
+
+        coupon = repository.save(coupon);
 
         return CouponMapper.toResponse(coupon);
     }
--- src/main/java/com/lucasduarte/cupon_api/utils/CouponMapper.java
+++ src/main/java/com/lucasduarte/cupon_api/utils/CouponMapper.java
@@ -1,7 +1,11 @@
 package com.lucasduarte.cupon_api.utils;
 
+import com.lucasduarte.cupon_api.dto.CouponRequestDTO;
 import com.lucasduarte.cupon_api.dto.CouponResponseDTO;
+import com.lucasduarte.cupon_api.enums.CouponStatus;
 import com.lucasduarte.cupon_api.model.Coupon;
+
+import java.time.LocalDateTime;
 
 public class CouponMapper {
 
@@ -15,6 +19,19 @@ public class CouponMapper {
                 .status(coupon.getStatus())
                 .published(coupon.isPublished())
                 .redeemed(coupon.isRedeemed())
+                .build();
+    }
+
+    public static Coupon toEntity(CouponRequestDTO dto) {
+        return Coupon.builder()
+                .code(dto.getCode())
+                .description(dto.getDescription())
+                .discountValue(dto.getDiscountValue())
+                .expirationDate(LocalDateTime.parse(dto.getExpirationDate()))
+                .status(CouponStatus.ACTIVE)
+                .published(dto.isPublished())
+                .redeemed(false)
+                .deleted(false)
                 .build();
     }
 }
