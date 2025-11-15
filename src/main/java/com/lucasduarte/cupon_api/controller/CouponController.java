bc0429c9a7d1a85c98b283bf71bb3ab254a86ee8
Lucas <lucas.duartebarbosa@outlook.com>
2025-11-15 16:36:31 -0300

    Feat: Criando o método create na controller CouponController

--- src/main/java/com/lucasduarte/cupon_api/controller/CouponController.java
+++ src/main/java/com/lucasduarte/cupon_api/controller/CouponController.java
@@ -1,13 +1,12 @@
 package com.lucasduarte.cupon_api.controller;
 
+import com.lucasduarte.cupon_api.dto.CouponRequestDTO;
 import com.lucasduarte.cupon_api.dto.CouponResponseDTO;
 import com.lucasduarte.cupon_api.service.CouponService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/coupon")
@@ -19,5 +18,11 @@ public class CouponController {
     @GetMapping("/{id}")
     public ResponseEntity<CouponResponseDTO> findById(@PathVariable String id) {
         return ResponseEntity.ok(service.findById(id));
+    }
+
+    @PostMapping
+    public ResponseEntity<CouponResponseDTO> create(@Valid @RequestBody CouponRequestDTO dto) {
+        CouponResponseDTO response = service.create(dto);
+        return ResponseEntity.status(201).body(response);
     }
 }
